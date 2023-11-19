fe4542be45bfeb3e5563fd8d8336bc19a80dd396
Nagel-P <130867271+Nagel-P@users.noreply.github.com>
2023-11-18 21:15:13 -0300

    Update jogodavelha-script.js
    finalização/indentação

--- jogodavelha-script.js
+++ jogodavelha-script.js
@@ -16,51 +16,51 @@ statusDisplay.innerHTML = turnoJogador();
 document.querySelectorAll('.casa').forEach(casa => casa.addEventListener('click', handleCellClick));
 document.querySelector('.game--restart').addEventListener('click', restartDeJogo);
 
-function handleCellClick(clickedCellEvent) {   
-        const casaClicada = clickedCellEvent.target;
-        const clickedCellIndex = parseInt(
-          casaClicada.getAttribute('data-casa-index')
-        );
-    
-        if (estadoPartida[clickedCellIndex] !== "" || !turnoAtivo) {
-            return;
-        }
-   
-        casaSelecionada(casaClicada, clickedCellIndex);
-        validacaoResultado();
+function handleCellClick(clickedCellEvent) {
+    const casaClicada = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(
+        casaClicada.getAttribute('data-casa-index')
+    );
+
+    if (estadoPartida[clickedCellIndex] !== "" || !turnoAtivo) {
+        return;
+    }
+
+    casaSelecionada(casaClicada, clickedCellIndex);
+    validacaoResultado();
 }
 
 function casaSelecionada(casaClicada, clickedCellIndex) {
-    
-        estadoPartida[clickedCellIndex] = jogadorAtual;
-        casaClicada.innerHTML = jogadorAtual;
-    }
 
-    const condicaoVitoria = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-    function validacaoResultado() {
-        let vitoriaDeRodada = false;
-        for (let i = 0; i < 8; i++) {
-            const condicaoControle = condicaoVitoria[i];
-            let a = estadoPartida[condicaoControle[0]];
-            let b = estadoPartida[condicaoControle[1]];
-            let c = estadoPartida[condicaoControle[2]];
-            if (a === '' || b === '' || c === '') {
-                continue;
-            }
-            if (a === b && b === c) {
-                vitoriaDeRodada = true;
-                break
-            }
+    estadoPartida[clickedCellIndex] = jogadorAtual;
+    casaClicada.innerHTML = jogadorAtual;
+}
+
+const condicaoVitoria = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+function validacaoResultado() {
+    let vitoriaDeRodada = false;
+    for (let i = 0; i < 8; i++) {
+        const condicaoControle = condicaoVitoria[i];
+        let a = estadoPartida[condicaoControle[0]];
+        let b = estadoPartida[condicaoControle[1]];
+        let c = estadoPartida[condicaoControle[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
         }
+        if (a === b && b === c) {
+            vitoriaDeRodada = true;
+            break
+        }
+    }
     if (vitoriaDeRodada) {
         statusDisplay.innerHTML = vitoria();
         turnoAtivo = false;
@@ -88,5 +88,5 @@ function restartDeJogo() {
     estadoPartida = ["", "", "", "", "", "", "", "", ""];
     statusDisplay.innerHTML = turnoJogador();
     document.querySelectorAll('.casa')
-               .forEach(casa => casa.innerHTML = "");
+        .forEach(casa => casa.innerHTML = "");
 }    
